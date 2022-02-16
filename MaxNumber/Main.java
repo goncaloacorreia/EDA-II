6790c67748a21857d92d5023ab0c05f6f1a30857
goncaloacorreia <gac632000@gmail.com>
2022-02-16 15:03:16 +0000

    aula1

---
+++ MaxNumber/Main.java
@@ -0,0 +1,30 @@
+package MaxNumber;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int children = Integer.parseInt(input.readLine());
+        int max = 0;
+        boolean checkMax = false;
+        for(int c = 0; c < children; c++){
+            String s []= input.readLine().split(" ");
+            
+            for(int aux = 1; aux < s.length; aux ++){
+               int sint = Integer.parseInt(s[aux]);
+               if(checkMax == false) {
+                   max = sint;
+                   checkMax = true;
+               }
+
+                if(sint > max){
+                    max = sint; 
+                }
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
