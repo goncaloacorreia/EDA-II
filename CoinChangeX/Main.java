58518a72d30f8ef4d0169831f88937c7f00ed32f
goncaloacorreia <gac632000@gmail.com>
2022-03-21 20:08:13 +0000

    a

---
+++ CoinChangeX/Main.java
@@ -0,0 +1,5 @@
+package CoinChangeX;
+
+public class Main {
+    
+}
--- ProjectFileDependencies/Main.java
+++ ProjectFileDependencies/Main.java
@@ -1,3 +1,4 @@
+package ProjectFileDependencies;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
