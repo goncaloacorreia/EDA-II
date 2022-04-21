b6c1c02122ecff8dc3b132538edc81aa29431645
goncaloacorreia <gac632000@gmail.com>
2022-04-21 14:18:48 +0100

    work3

---
+++ Work2/Main.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.awt.Point;
+
+public class Main{
+
+    //função da distância entre dois pontos
+    public static double distance(Point A, Point B)
+    {
+        double dist = Math.sqrt((B.x - A.x) * 2 + (B.y - A.y) * 2);
+
+        return dist;
+    }
+
+    //Sort para os pontos
+    public static void quickSort(Point[] arr, int start, int end){
+ 
+        int partition = partition(arr, start, end);
+ 
+        if(partition-1>start) {
+            quickSort(arr, start, partition - 1);
+        }
+        if(partition+1<end) {
+            quickSort(arr, partition + 1, end);
+        }
+    }
+ 
+    public static int partition(Point[] arr, int start, int end){
+        int pivot = arr[end].y;
+        Point pivot1 = arr[end];
+ 
+        for(int i=start; i<end; i++){
+            if(arr[i].y<pivot){
+                Point temp= arr[start];
+                arr[start]=arr[i];
+                arr[i]=temp;
+                start++;
+            }
+        }
+ 
+        Point temp = arr[start];
+        arr[start] = pivot1;
+        arr[end] = temp;
+ 
+        return start;
+    }
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String s[] = input.readLine().split(" ");
+
+        int points = Integer.parseInt(s[0]);
+        int height = Integer.parseInt(s[1]);
+        int cases = Integer.parseInt(s[2]);
+
+        // lista dos pontos
+        Point new_points[] = new Point[points];
+
+        
+        //armazenar pontos
+        for(int aux = 0; aux<points; aux++)
+        {
+            String single_point[] = input.readLine().split(" ");
+            int x = Integer.parseInt(single_point[0]);
+            int y = Integer.parseInt(single_point[1]);
+
+            new_points[aux] = new Point(x, y);
+
+
+        }
+        //Realiza se o sort dos pontos
+        quickSort(new_points, 0, new_points.length-1);
+
+
+        for(int aux1 = 0; aux1<points; aux1++)
+        {
+            System.out.println(new_points[aux1]);
+
+
+        }
+
+        
+
+
+
+    }
+
+}
