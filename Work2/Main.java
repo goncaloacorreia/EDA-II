0ddfe84b6a47140813d3c18271ba40cd540f0d66
goncaloacorreia <gac632000@gmail.com>
2022-04-27 20:57:33 +0100

    work3DONE

--- Work2/Main.java
+++ Work2/Main.java
@@ -1,17 +1,20 @@
 import java.io.*;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.*;
 
+class PointCmp implements Comparator<Point> {
+    public int compare(Point a, Point b) {
+        return (a.y < b.y) ? -1 : (a.y > b.y) ? 1 : 0;
+    }
+}
 
 public class Main {
 
     // função da distância entre dois pontos
     public static double distance(Point A, Point B, double reach) {
         double dist = Math.sqrt(Math.pow((B.x - A.x), 2) + Math.pow((B.y - A.y), 2));
-        //double dist = Math.sqrt((B.y - A.y) * (B.y - A.y) + (B.x - A.x) * (B.x - A.x));
 
         if(dist > reach)
         {
@@ -21,41 +24,106 @@ public class Main {
         return dist;
     }
 
-    // Sort para os pontos
-    public static void quickSort(Point[] arr, int start, int end) {
+    public static int printLevels(ArrayList<ArrayList<Integer>> graph, int V, int x, int height, double reach, Point new_points[])
+    {   
+        boolean unreachable = true;
+        int min = 30001;
+        int z;
 
-        int partition = partition(arr, start, end);
+        if(reach >= height)
+        {
 
-        if (partition - 1 > start) {
-            quickSort(arr, start, partition - 1);
+            return 0;
+
         }
-        if (partition + 1 < end) {
-            quickSort(arr, partition + 1, end);
+
+        // array para cada nivel do node
+        int level[] = new int[V];
+        boolean marked[] = new boolean[V];
+
+        // criar a queue
+        Queue<Integer> que = new LinkedList<Integer>();
+        for(int y = 0; y <= x; y++){
+            // dar enqueue em cada ponto inicial
+            que.add(y);
+
+            // nivel do node fonde inicializado a 0
+            level[y] = 1;
+
+            // marcado como visitado
+            marked[y] = true;
         }
-    }
+        while (que.size() > 0)
+        {   
+            
+            // primeiro elemento da queue
+            
+            z = que.peek();
 
-    public static int partition(Point[] arr, int start, int end) {
-        int pivot = arr[end].y;
-        Point pivot1 = arr[end];
+            // dequeue elemento
+            que.remove();
+            
+            if(new_points[z].y + reach >= height)
+                    {   
+                        unreachable = false;
+                        if(level[z] < min)
+                        {
+                            min = level[z];
+                        }
+                    }
+            // percorrer os adjacentes do node y
+            for (int i = 0; i < graph.get(z).size(); i++)
+            {
+                
+                // cada vizinho
+                int b = graph.get(z).get(i);
 
-        for (int i = start; i < end; i++) {
-            if (arr[i].y < pivot) {
-                Point temp = arr[start];
-                arr[start] = arr[i];
-                arr[i] = temp;
-                start++;
+                if(distance(new_points[z], new_points[b], reach) == -1)
+                {   
+                    continue;
+                }
+                
+                // se b não estiver marcado, será adicionado à queue
+                if (!marked[b])
+                {
+
+                    // enqueue b in queue
+                    que.add(b);
+                    
+                    // nivel do vertice = nivel do adjacente + 1
+                    level[b] = level[z] + 1;
+                    if(new_points[b].y + reach >= height)
+                    {   
+                        unreachable = false;
+                        if(level[b] < min)
+                        {
+                            min = level[b];
+                        }
+                    }
+
+                    // marcar b
+                    marked[b] = true;
+                }
             }
         }
+        
+        //não encontrou nenhum ponto em que conseguisse chegar ao topo
+        if(unreachable == true)
+        {
 
-        Point temp = arr[start];
-        arr[start] = pivot1;
-        arr[end] = temp;
+            return -1;
+            
+        }
 
-        return start;
+        //encontra pelo menos um caminho e retorna o menor
+        else{
+            
+            return min;
+
+        }
     }
-    
+
     public static void main(String[] args) throws IOException {
-        long start = System.currentTimeMillis();
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
         String s[] = input.readLine().split(" ");
@@ -64,11 +132,7 @@ public class Main {
         int height = Integer.parseInt(s[1]);
         int cases = Integer.parseInt(s[2]);
 
-        int counter = 150;
-        if(points < 150){
-            counter = points;
-        }
-        // lista dos pontos
+        // array dos pontos
         Point new_points[] = new Point[points];
 
         // armazenar pontos
@@ -80,109 +144,100 @@ public class Main {
             new_points[aux] = new Point(x, y);
 
         }
-        // Realiza-se o sort dos pontos
-        //quickSort(new_points, 0, new_points.length - 1);
-        PointCmp z = new PointCmp();
 
+        //Sort dos pontos de acordo com a ordenada
         Arrays.sort(new_points, new PointCmp());
         
+        //encontrar o maior reach
+        double maxreach = -1;
+        double reach[] = new double [cases];
+
+        for(int reachindex = 0; reachindex < cases; reachindex++)
+        {    
+            reach[reachindex] = Integer.parseInt(input.readLine());
+            if(reach[reachindex] > maxreach)
+            {
+                maxreach = reach[reachindex];
+            }
+        }
+
         
-        //BFS.Graph g = new BFS.Graph(points);
-        BFS2 g = new BFS2();
-
-        for (; cases > 0; cases--) {
-
-            double reach = Integer.parseInt(input.readLine());
-            //criação do grafo
-            ArrayList<ArrayList<Integer>> adj =
-                        new ArrayList<ArrayList<Integer>>(points);
-            for (int i = 0; i < points; i++) {
-                adj.add(new ArrayList<Integer>());
-            }
-            
-            for (int aux1 = 0; aux1 < points; aux1++) {
-                for (int aux2 = aux1 + 1; aux2 < counter; aux2++) {
-                    double dist = distance(new_points[aux1], new_points[aux2], reach);
-
-                    if(dist == -1)
-                    {
-                        continue;
-                    }
-                    else{
-                        g.addEdge(adj, aux1, aux2);
-                        //g.addEdge(adj, aux2, aux1);
-                    }
-
-                }
-            }
-            
-            boolean bench = false;
-            
-            if(reach >= height){
-                bench = true;
-            }
-
-            //verificar quais os pontos iniciais
-            int max_initials = -1;
-            for(int aux3 = 0; aux3 < points; aux3++){
-                if(new_points[aux3].y <= reach){
-                    max_initials = aux3;
-                }
-                else{
-                    break;
-                }
-            }
-            //verificar quais os pontos finais
-            int min_finals = -1;
-            for(int aux3 = points - 1; aux3 != -1; aux3--){
-                if( (height - new_points[aux3].y) <= reach){
-                    min_finals = aux3;
-                }
-                else{
-                    break;
-                }
-            }
-            
-            boolean unreachable = false;
-            if(min_finals == -1){
-                unreachable = true;
-            }
-
-            if(max_initials >= 35){
-                max_initials = 35;
-            }
-            
-            //encontrar o caminho mais curto
-            int short_path = 2147483647;
-            if(unreachable == false){
-                for(int aux4 = 0; aux4 <= max_initials; aux4++){
-                    //System.out.println("Initial: " + aux4);
-                    for(int aux5 = min_finals; aux5 < points; aux5++){
-                        //System.out.println("Final: " + aux5);
-                        int shrt = g.ShortestDistance(adj, aux4, aux5, points);
-                        if(shrt < short_path){
-                            short_path = shrt;
-                        }
-                    }
-                } 
-            }
-            
-            short_path += 1;
-            if(bench == true){
-                System.out.println("0");
-            }
-            else if(unreachable == true){
-                System.out.println("unreachable");
-            }
-            else{
-                System.out.println(short_path);
-            }
-            
+        //criação do grafo
+        ArrayList<ArrayList<Integer>> adj =
+                    new ArrayList<ArrayList<Integer>>(points);
+        for (int i = 0; i < points; i++) {
+            adj.add(new ArrayList<Integer>());
         }
         
-        long end = System.currentTimeMillis();
-        NumberFormat formatter = new DecimalFormat("#0.00000");
-        //System.out.println("Time: " + formatter.format((end - start) / 1000d) + " seconds");
-    }
+        for (int aux1 = 0; aux1 < points; aux1++) {
+            for (int aux2 = aux1 + 1; aux2 < points; aux2++) {
+                if(new_points[aux2].y - new_points[aux1].y > maxreach){
+                    break;
+                }
+                else{
+                    double dist = distance(new_points[aux1], new_points[aux2], maxreach);
+                    if(dist != -1)
+                    {
+                        adj.get(aux1).add(aux2);
+                        adj.get(aux2).add(aux1);
+                    }
+                }
+            }
+        }
+        
+        //verificar se existe um caminho
+        boolean path = false;
+        for(int i = 0; i < points; i++){
+            if(new_points[i].y + maxreach >= height){
+                path = true;
+                break;
+            }
+        }
 
+        //se não existir um caminho para maxreach, printar unreachable ou 0, caso seja possível chegar a height sem precisar de qualquer ponto, para os restantes cases
+        if(path == false){
+            for(int caseindex = 0; caseindex < cases; caseindex++){
+                if(reach[caseindex] >= height){
+                    System.out.println("0");
+                }else{
+                    System.out.println("unreachable");
+                }
+            }
+        }
+        else
+        {
+            //verificar cada reach
+            for(int caseindex = 0; caseindex < cases; caseindex++)
+            {   
+                double new_reach = reach[caseindex];
+                
+                //calcular valores iniciais para este reach
+                int max_initials = -1;
+                for(int aux3 = 0; aux3 < points; aux3++){
+                    if(new_points[aux3].y <= new_reach){
+                        max_initials = aux3;
+                    }
+                    else{
+                        break;
+                    }
+                }
+
+                //caso não haja valores iniciais
+                if(max_initials == -1 && new_reach < height){
+                    System.out.println("unreachable");
+                } else if(max_initials == -1 && new_reach >= height){
+                    System.out.println(0);
+                }
+                else{
+                    //calcular caminho mais curto para cada valor inicial
+                    int aux5 = printLevels(adj, points, max_initials, height, new_reach, new_points);
+                    if(aux5 == -1){
+                        System.out.println("unreachable");
+                    }else{
+                        System.out.println(aux5);
+                    }    
+                }
+            }
+        } 
+    }
 }
--- Work2New/PointCmp.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-import java.awt.Point;
-
-class PointCmp implements Comparator<Point> {
-    public int compare(Point a, Point b) {
-        return (a.y < b.y) ? -1 : (a.y > b.y) ? 1 : 0;
-    }
-}
