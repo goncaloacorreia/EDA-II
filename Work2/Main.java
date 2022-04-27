10ddce3334155cf248de1fba16db2e919df64712
goncaloacorreia <gac632000@gmail.com>
2022-04-27 22:10:41 +0100

    work3DONE

--- Work2/Main.java
+++ Work2/Main.java
@@ -24,11 +24,11 @@ public class Main {
         return dist;
     }
 
-    public static int printLevels(ArrayList<ArrayList<Integer>> graph, int V, int x, int height, double reach, Point new_points[])
+    public static int printLevels(ArrayList<ArrayList<Integer>> graph, int nVertex, int max_initial, int height, double reach, Point points[])
     {   
         boolean unreachable = true;
         int min = 30001;
-        int z;
+        int queueAux;
 
         if(reach >= height)
         {
@@ -38,13 +38,14 @@ public class Main {
         }
 
         // array para cada nivel do node
-        int level[] = new int[V];
-        boolean marked[] = new boolean[V];
+        int level[] = new int[nVertex];
+        boolean marked[] = new boolean[nVertex];
 
         // criar a queue
         Queue<Integer> que = new LinkedList<Integer>();
-        for(int y = 0; y <= x; y++){
-            // dar enqueue em cada ponto inicial
+        for(int y = 0; y <= max_initial; y++)
+        {
+            // enqueue em cada ponto inicial
             que.add(y);
 
             // nivel do node fonde inicializado a 0
@@ -57,28 +58,27 @@ public class Main {
         {   
             
             // primeiro elemento da queue
-            
-            z = que.peek();
+            queueAux = que.peek();
 
             // dequeue elemento
             que.remove();
             
-            if(new_points[z].y + reach >= height)
-                    {   
-                        unreachable = false;
-                        if(level[z] < min)
-                        {
-                            min = level[z];
-                        }
-                    }
+            if(points[queueAux].y + reach >= height)
+            {   
+                unreachable = false;
+                if(level[queueAux] < min)
+                {
+                    min = level[queueAux];
+                }
+            }
             // percorrer os adjacentes do node y
-            for (int i = 0; i < graph.get(z).size(); i++)
+            for (int i = 0; i < graph.get(queueAux).size(); i++)
             {
                 
-                // cada vizinho
-                int b = graph.get(z).get(i);
+                // cada adjacente
+                int b = graph.get(queueAux).get(i);
 
-                if(distance(new_points[z], new_points[b], reach) == -1)
+                if(distance(points[queueAux], points[b], reach) == -1)
                 {   
                     continue;
                 }
@@ -91,8 +91,8 @@ public class Main {
                     que.add(b);
                     
                     // nivel do vertice = nivel do adjacente + 1
-                    level[b] = level[z] + 1;
-                    if(new_points[b].y + reach >= height)
+                    level[b] = level[queueAux] + 1;
+                    if(points[b].y + reach >= height)
                     {   
                         unreachable = false;
                         if(level[b] < min)
@@ -110,43 +110,40 @@ public class Main {
         //não encontrou nenhum ponto em que conseguisse chegar ao topo
         if(unreachable == true)
         {
-
             return -1;
-            
         }
 
         //encontra pelo menos um caminho e retorna o menor
-        else{
-            
+        else
+        {
             return min;
-
         }
     }
 
     public static void main(String[] args) throws IOException {
+        //leitura do input
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-
         String s[] = input.readLine().split(" ");
-
-        int points = Integer.parseInt(s[0]);
+        int npoints = Integer.parseInt(s[0]);
         int height = Integer.parseInt(s[1]);
         int cases = Integer.parseInt(s[2]);
 
         // array dos pontos
-        Point new_points[] = new Point[points];
+        Point points[] = new Point[npoints];
 
         // armazenar pontos
-        for (int aux = 0; aux < points; aux++) {
+        for (int aux = 0; aux < npoints; aux++) 
+        {
             String single_point[] = input.readLine().split(" ");
             int x = Integer.parseInt(single_point[0]);
             int y = Integer.parseInt(single_point[1]);
 
-            new_points[aux] = new Point(x, y);
+            points[aux] = new Point(x, y);
 
         }
 
         //Sort dos pontos de acordo com a ordenada
-        Arrays.sort(new_points, new PointCmp());
+        Arrays.sort(points, new PointCmp());
         
         //encontrar o maior reach
         double maxreach = -1;
@@ -163,19 +160,24 @@ public class Main {
 
         
         //criação do grafo
-        ArrayList<ArrayList<Integer>> adj =
-                    new ArrayList<ArrayList<Integer>>(points);
-        for (int i = 0; i < points; i++) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(npoints);
+
+        for (int i = 0; i < npoints; i++) 
+        {
             adj.add(new ArrayList<Integer>());
         }
         
-        for (int aux1 = 0; aux1 < points; aux1++) {
-            for (int aux2 = aux1 + 1; aux2 < points; aux2++) {
-                if(new_points[aux2].y - new_points[aux1].y > maxreach){
+        for (int aux1 = 0; aux1 < npoints; aux1++) 
+        {
+            for (int aux2 = aux1 + 1; aux2 < npoints; aux2++) 
+            {
+                if(points[aux2].y - points[aux1].y > maxreach)
+                {
                     break;
                 }
-                else{
-                    double dist = distance(new_points[aux1], new_points[aux2], maxreach);
+                else
+                {
+                    double dist = distance(points[aux1], points[aux2], maxreach);
                     if(dist != -1)
                     {
                         adj.get(aux1).add(aux2);
@@ -187,19 +189,25 @@ public class Main {
         
         //verificar se existe um caminho
         boolean path = false;
-        for(int i = 0; i < points; i++){
-            if(new_points[i].y + maxreach >= height){
+        for(int i = 0; i < npoints; i++)
+        {
+            if(points[i].y + maxreach >= height)
+            {
                 path = true;
                 break;
             }
         }
 
-        //se não existir um caminho para maxreach, printar unreachable ou 0, caso seja possível chegar a height sem precisar de qualquer ponto, para os restantes cases
-        if(path == false){
-            for(int caseindex = 0; caseindex < cases; caseindex++){
+        //se não existir um caminho para maxreach, printar unreachable ou 0(caso seja possível chegar a height sem precisar de qualquer ponto), para os restantes cases
+        if(path == false)
+        {
+            for(int caseindex = 0; caseindex < cases; caseindex++)
+            {
                 if(reach[caseindex] >= height){
                     System.out.println("0");
-                }else{
+                }
+                else
+                {
                     System.out.println("unreachable");
                 }
             }
@@ -207,33 +215,42 @@ public class Main {
         else
         {
             //verificar cada reach
-            for(int caseindex = 0; caseindex < cases; caseindex++)
+            for(int reachindex = 0; reachindex < cases; reachindex++)
             {   
-                double new_reach = reach[caseindex];
+                double new_reach = reach[reachindex];
                 
                 //calcular valores iniciais para este reach
                 int max_initials = -1;
-                for(int aux3 = 0; aux3 < points; aux3++){
-                    if(new_points[aux3].y <= new_reach){
+                for(int aux3 = 0; aux3 < npoints; aux3++)
+                {
+                    if(points[aux3].y <= new_reach)
+                    {
                         max_initials = aux3;
                     }
-                    else{
+                    else
+                    {
                         break;
                     }
                 }
 
                 //caso não haja valores iniciais
-                if(max_initials == -1 && new_reach < height){
+                if(max_initials == -1 && new_reach < height)
+                {
                     System.out.println("unreachable");
-                } else if(max_initials == -1 && new_reach >= height){
+                } 
+                else if(max_initials == -1 && new_reach >= height)
+                {
                     System.out.println(0);
                 }
-                else{
-                    //calcular caminho mais curto para cada valor inicial
-                    int aux5 = printLevels(adj, points, max_initials, height, new_reach, new_points);
-                    if(aux5 == -1){
+                else
+                {
+                    //calcular caminho mais curto para cada valor inicial ou unreachable, caso não seja possível chegar ao topo
+                    int aux5 = printLevels(adj, npoints, max_initials, height, new_reach, points);
+                    if(aux5 == -1)
+                    {
                         System.out.println("unreachable");
-                    }else{
+                    }else
+                    {
                         System.out.println(aux5);
                     }    
                 }
